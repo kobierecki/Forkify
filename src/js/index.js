7474b3496529e8347eb769de1dcbe97b83017964
Oskar Kobierecki/WAW/PL <oskar.kobierecki@avon.com>
2019-03-12 16:10:37 +0100

    working API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
-// Global app controller
-import num from './test';
-const x =23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+import axios from 'axios';
+
+  async function getResults(query) {
+    const key = '8cfdc815b922fe7f01f91462c3b0f99c';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    console.log(res);
+  }
+
+  getResults('pizza');
