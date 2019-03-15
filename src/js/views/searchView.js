c1939b061c1e18a7506def1eb0b1931e097ac136
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2019-03-15 23:52:12 +0100

    Added recipe title reducer

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,20 +10,32 @@ export const clearResults = () => {
   elements.searchResultList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, curr) => {
+            if (acc + curr.length <= limit){
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+        },0);
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
-    const markup = `
-<li>
+    const markup = `<li>
                     <a class="results__link" href="#${recipe.recipe_id}">
                         <figure class="results__fig">
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
-                </li>
-`;
+                </li>`;
     elements.searchResultList.insertAdjacentHTML("beforeend", markup);
 };
 
